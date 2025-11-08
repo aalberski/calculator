7b55898bd538dacb1978965ebc01f78fa0e29011
Adam Alberski <alberskiadam.t@gmail.com>
2025-11-07 23:15:18 -0500

    Created basic functioning calculator, can add, subtract, multiply, divide.

--- script.js
+++ script.js
@@ -1,20 +1,97 @@
 const display = document.querySelector(".display");
 const calculator = document.querySelector(".calculator");
-const digits = document.querySelectorAll('.digit');
-const divide = document.querySelector('#divide');
-const multiply = document.querySelector('#multiply');
-const subtract = document.querySelector('#subtract');
-const add = document.querySelector('#add');
-const backspace = document.querySelector('#backspace');
-const clear = document.querySelector('#clear');
-const enter = document.querySelector('#enter');
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const decimal = document.querySelector("#decimal");
+const del = document.querySelector("#delete");
+
+let left = "";
+let right = "";
+let lastOperator = null;
+
+
+function calculate(left, right, operator){
+    if(right == ""){
+        return left;
+    }
+    switch(operator){
+        case("divide"):
+            if(right == 0){
+                alert("You know you can't do that.");
+                left = "";
+                right = "";
+                lastOperator = "";
+                break;
+            }
+            return Number(left) / Number(right);
+        case("multiply"):
+            return Number(left) * Number(right);
+        case("subtract"):
+            return Number(left) - Number(right);
+        case("add"):
+            return Number(left) + Number(right);
+    }
+}
 
 digits.forEach((digit) =>{
     digit.addEventListener('click', () =>{
-        alert(digit.textContent);
+        if(lastOperator == null){
+            left = left + digit.textContent;
+            display.textContent = left;
+        }
+        else{
+            right = right + digit.textContent;
+            display.textContent = right;
+        }
     })
 })
 
-divide.addEventListener('click', ()=>{
+operators.forEach((operator) =>{
+    operator.addEventListener('click', () =>{
+        lastOperator = operator.id;
+        right = "";
+        display.textContent = operator.textContent;
+    })
+})
 
+equals.addEventListener('click', ()=>{
+    left = calculate(left, right, lastOperator);
+    if(left != null){
+        display.textContent = left;
+    }
+})
+
+clear.addEventListener('click', ()=>{
+    left = "";
+    right = "";
+    lastOperator = null;
+    display.textContent = '0';
+})
+
+decimal.addEventListener('click', ()=>{
+    if(lastOperator == null && !left.includes('.')){
+        left = left + "."
+        display.textContent = left;
+    }
+    else if(lastOperator != null && !right.includes('.')){
+        right = right + "."
+        display.textContent = right;
+    }
+})
+
+del.addEventListener('click', ()=>{
+    if(right != ""){
+        right = right.slice(0, -1);
+        display.textContent = right;
+    }
+    else if(lastOperator != null){
+        lastOperator = null;
+        display.textContent = left;
+    }
+    else if(left != ""){
+        left = left.slice(0, -1);
+        display.textContent = left;
+    }
 })
